f9ab19d0a06bb69b7bb831d4285a0a2bf17fe2cb
Ali Shokair <alishkeir_@hotmail.com>
2026-02-11 20:28:21 +0200

    Created minimal Dashbaord view to show Tasks

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,7 @@
-<template></template>
+<script setup lang="ts">
+import DashboardView from './views/DashboardView.vue';
+</script>
+
+<template>
+  <DashboardView />
+</template>
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-view">
+    <h1>Dashboard</h1>
+
+    <!-- TODO: use spinner component-->
+    <div v-if="store.isLoading">Loading...</div>
+
+    <TaskList v-else :tasks="store.tasks" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useTaskStore } from '@/stores/taskStore';
+import TaskList from '@/components/tasks/TaskList.vue';
+
+const store = useTaskStore();
+
+// fetch the data as soonas the page loads
+onMounted(() => {
+  store.fetchTasks();
+});
+</script>
+
+<style scoped>
+.dashboard-view {
+  padding: 2rem;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
