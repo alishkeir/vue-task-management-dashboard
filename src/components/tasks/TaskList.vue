e1fc2b93cc81b48bc94ac8806752589600bc418f
Ali Shokair <alishkeir_@hotmail.com>
2026-02-11 20:16:40 +0200

    Updated TaskList to use TaskCard and show tasks

---
+++ src/components/tasks/TaskList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="task-list">
+    <TaskCard v-for="task in tasks" :key="task.id" :task="task" />
+
+    <p v-if="tasks.length === 0">No tasks to show!</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ITask } from '@/types/task';
+import TaskCard from './TaskCard.vue';
+
+defineProps<{
+  tasks: ITask[];
+}>();
+</script>
+
+<style scoped>
+.task-list {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
