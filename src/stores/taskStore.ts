3f0a524d91f8085612318adf4494e7e567acb18e
Ali Shokair <alishkeir_@hotmail.com>
2026-02-11 19:48:12 +0200

    Aded pinia store to fetch and update tasks

--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,1 +1,34 @@
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { ITask, TTaskStatus } from '@/types/task';
+import { taskService } from '@/services/taskService';
 
+export const useTaskStore = defineStore('task', () => {
+  const tasks = ref<ITask[]>([]); // reactive tasks array (data)
+  const isLoading = ref(false);
+
+  //   fetch thetasks from the service and update the status
+  const fetchTasks = async () => {
+    isLoading.value = true; //adding this here to global ui sync, instead of waiting for the api call delay
+    try {
+      tasks.value = await taskService.fetchTasks();
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  //   update the task status in the store using the new value
+  const updateTaskStatus = (taskId: string | number, newStatus: TTaskStatus) => {
+    const task = tasks.value.find((task) => task.id === taskId);
+    if (task) {
+      task.status = newStatus;
+    }
+  };
+
+  return {
+    tasks,
+    isLoading,
+    fetchTasks,
+    updateTaskStatus,
+  };
+});
