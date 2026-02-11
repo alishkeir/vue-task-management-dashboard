5359f707ee6eda647082358e9c6a49a6b5d57175
Ali Shokair <alishkeir_@hotmail.com>
2026-02-11 22:02:09 +0200

    Added Button component to TaskCard to update task status

--- src/components/tasks/TaskCard.vue
+++ src/components/tasks/TaskCard.vue
@@ -7,6 +7,13 @@
       <p>{{ task.assignee }}</p>
     </div>
 
+    <div class="task-status">
+      <h4>Update Status:</h4>
+      <Button v-if="task.status !== 'todo'" @click="handleStatusChange('todo')" status="todo" text="Todo" />
+      <Button v-if="task.status !== 'in_progress'" @click="handleStatusChange('in_progress')" status="in_progress" text="In Progress" />
+      <Button v-if="task.status !== 'done'" @click="handleStatusChange('done')" status="done" text="Done" />
+    </div>
+
     <div class="task-footer">
       <CardBadge :text="task.status" type="status" />
       <CardBadge :text="task.priority" type="priority" />
@@ -15,12 +22,20 @@
 </template>
 
 <script setup lang="ts">
-import type { ITask } from '@/types/task';
+import type { ITask, TTaskStatus } from '@/types/task';
 import CardBadge from '../common/CardBadge.vue';
 
-defineProps<{
+import { useTaskStore } from '@/stores/taskStore';
+import Button from '../common/Button.vue';
+const props = defineProps<{
   task: ITask;
 }>();
+
+const store = useTaskStore();
+
+const handleStatusChange = (newStatus: TTaskStatus) => {
+  store.updateTaskStatus(props.task.id, newStatus);
+};
 </script>
 
 <style scoped>
@@ -42,6 +57,12 @@ defineProps<{
 
 .task-assignee img {
   width: 25px;
+}
+.task-status {
+  display: flex;
+  gap: 8px;
+  padding: 1rem 0;
+  border-bottom: 1px solid #ccc;
 }
 
 .task-footer {
